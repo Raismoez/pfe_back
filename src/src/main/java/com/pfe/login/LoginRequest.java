58df52279effa2abe8dbed303741b90fc9ed2cda
Meriam Daadaa <meriamdaadaa8@gmail.com>
2025-04-28 11:48:08 +0200

    qaa

--- src/src/main/java/com/pfe/login/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.pfe.login;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
-}
--- src/src/main/java/com/pfe/login/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pfe.login.repository;
-
-import com.pfe.login.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Role findByRoleName(String roleName);
-}
